6c51734db9cac4764c7cdcec323684cb8fda7194
yaroslavvyhivskyi <iarick.vigasi@gmail.com>
2017-11-25 20:08:25 +0200

    Clean project

--- src/App.js
+++ src/App.js
@@ -1,76 +1,98 @@
 import React, { Component } from 'react';
-import 'react-big-calendar/lib/css/react-big-calendar.css';
-import './App.css';
-import Main from './Main'
+import firebase from './firebase.js';
+import './App_s.css';
 
-/*
-class TodoList extends React.Component {
-  constructor(props, context) {
-    super(props, context);
-
-this.state = {
-  items: []
-}
-    this.addItem = this.addItem.bind(this);
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      klas: '',
+      tema: '',
+      items: []
+    }
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
   }
-  addItem(e){
+  componentDidMount() {
+  const itemsRef = firebase.database().ref('items');
+  itemsRef.on('value', (snapshot) => {
+    let items = snapshot.val();
+    let newState = [];
+    for (let item in items) {
+      newState.push({
+        id: item,
+        main_tema: items[item].main_tema,
+        main_title: items[item].main_title
+      });
+    }
+    this.setState({
+      items: newState
+    });
+  });
+}
+  handleSubmit(e) {
+    e.preventDefault();
+    const itemsRef = firebase.database().ref('items');
+    const item = {
+      main_tema: this.state.tema,
+      main_title: this.state.klas
+    }
+    itemsRef.push(item);
+    this.setState({
+      tema: '',
+      klas: ''
+    });
+  }
+  handleChange(e) {
+    this.setState({
+      [e.target.name]: e.target.value
+    });
+    console.log(e.target.value)
 
+  }
+  removeItem(itemId) {
+  const itemRef = firebase.database().ref(`/items/${itemId}`);
+  itemRef.remove();
   }
   render() {
     return (
-      <div className="todoListMain">
-        <div className="header">
-          <form onSubmit={this.addItem}>
-            <input placeholder="enter task">
-            </input>
-            <button type="submit">add</button>
-          </form>
+      <div className='app'>
+        <header>
+            <div className='wrapper'>
+              <h1>Добавить класс и тему</h1>
+
+            </div>
+        </header>
+        <div className='container'>
+        <section className="add-item">
+        <form onSubmit={this.handleSubmit}>
+          <input type="number" name="klas" placeholder="Класс" onChange={this.handleChange} value={this.state.klas} />
+          <input type="text" name="tema" placeholder="Тема" onChange={this.handleChange} value={this.state.tema} />
+          <button>Добавить</button>
+        </form>
+        </section>
+        <section className='display-item'>
+          <div className="wrapper">
+            <ul>
+              {this.state.items.map((item) => {
+                return (
+                  <li key={item.id}>
+                    <h3>{item.main_title} класс</h3>
+                    <p>Тема: {item.main_tema}
+                    <div className='but'>
+                      <button onClick={() => this.removeItem(item.id)}>Удалить</button>
+                      <button >Добавить файлы</button>
+                    </div>
+                    </p>
+                  </li>
+                )
+              })}
+            </ul>
+          </div>
+        </section>
         </div>
       </div>
     );
   }
-};
-
-export default TodoList;
-
-*/
-
-
-class App extends Component {
-  constructor(props){
-    super(props)
-    this.state={ test: 'test',
-                events: [
-                  {
-                      start: '2015-07-20',
-                      end: '2015-07-02',
-                      eventClasses: 'optionalEvent',
-                      title: 'test event',
-                      description: 'This is a test description of an event',
-                  },
-                  {
-                      start: '2015-07-19',
-                      end: '2015-07-25',
-                      title: 'test event',
-                      description: 'This is a test description of an event',
-                      data: 'you can add what ever random data you may want to use later',
-                  }
-                ]
-              }
-  }
-  componetnDidUpdate(){
-
-  }
-  componentDidMount(){
-
-  }
-  render() {
-    return (
-      <div>
-        <Main></Main>
-      </div>
-    );
-  }
 }
-
 export default App;
--- src/index.js
+++ src/index.js
@@ -2,11 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import App_s from './App_s';
-import logo from './logo.svg';
 import firebase from './firebase.js';
 import registerServiceWorker from './registerServiceWorker';
 
 
-ReactDOM.render(<App_s />, document.getElementById('root'));
+ReactDOM.render(<App/>, document.getElementById('root'));
 registerServiceWorker();
