4ca0ab2560c5c9e1f294c042cbc745a07cb6373c
TtomaS7 <ttoma8171@gmail.com>
2017-12-03 23:56:09 +0200

    Authentication and authorization

--- src/components/AddLesson.jsx
+++ src/components/AddLesson.jsx
@@ -1,9 +1,28 @@
 import React from 'react';
 
 import swal from 'sweetalert'
+import firebase from 'firebase';
 import  '../css/App_s.css'
 
 export default class AddLesson extends React.Component {
+  constructor() {
+    super();
+
+    const provider = new firebase.auth.FacebookAuthProvider();
+
+    firebase.auth().onAuthStateChanged(firebaseUser => {
+      if (firebaseUser){
+        console.log(firebaseUser);
+
+      } else {
+        console.log('not logged in');
+      }
+    })
+
+    this.state = {
+      user: null
+    }
+  }
   onLessonAdd = () => {
     if (!this.className.value || !this.topic.value) {
       swal("Oops...", "Something went wrong! Please, add class or topic.", "error");
@@ -17,9 +36,44 @@ export default class AddLesson extends React.Component {
     })
   }
 
+  onLogIn = () => {
+    const  email = this.txtLogin.value;
+    const  pass = this.txtPassword.value;
+    const  auth = firebase.auth();
+
+    const promise = auth.signInWithEmailAndPassword(email, pass);
+    promise.catch(e => console.log(e.message));
+  }
+
+  onSignUp = () => {
+    const  email = this.txtLogin.value;
+    const  pass = this.txtPassword.value;
+    const  auth = firebase.auth();
+
+    const promise = auth.createUserWithEmailAndPassword(email, pass);
+    promise.catch(e => console.log(e.message));
+  }
+
+  onSignOut = () => {
+    firebase.auth().signOut();
+  }
+
   render() {
     return (
       <div>
+        <input className="main_input_login" ref={el => {this.txtLogin = el}} type="text" placeholder="Login" />
+        <br/>
+        <input className="main_input_password" ref={el => {this.txtPassword = el}} type="text"  placeholder="Password" />
+        <br/>
+        <button onClick={this.onLogIn}>Log In</button>
+        <br/>
+        <button onClick={this.onSignUp}>Sign Up</button>
+        <br/>
+        <button className="logOut" onClick={this.onSignOut}>Sign Out</button>
+        <br/>
+        <br/>
+        <br/>
+        <br/>
         <input className="main_input_class" ref={el => {this.className = el}} type="text" placeholder="Класс" />
         <br/>
         <input className="main_input_topic" ref={el => {this.topic = el}} type="text"  placeholder="Тема" />
